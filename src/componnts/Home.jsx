31b026baf12a4cfdc802c989c2662d701183d24f
Madonna-Mansour <madonnamansour57@gmail.com>
2025-02-14 15:50:53 +0200

    Update Home.jsx

--- src/componnts/Home.jsx
+++ src/componnts/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Col ,Row , Container} from 'react-bootstrap'
 import download from '../imges/download.png'
 import me from '../imges/me.png'
-import Madonna from '../componnts/Madonna.pdf'
+import Madonna from '../componnts/cv.pdf'
 
 const Home = () => {
   return (
